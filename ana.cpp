aa726384bef58fbf3d012c77030196dc114b4e24
justinlaykovich <jdlb8d@mail.umkc.edu>
2016-10-06 11:41:07 -0500

    Inaccurate comments.

--- ana.cpp
+++ ana.cpp
@@ -5,10 +5,13 @@ using std::string;
 using std::cout;
 using std::endl;
 
+/* Main function */
 void ana(const string& suffix, const string& word) {
    size_t length = word.length();
 
-   // Base cases.
+   /* Base cases:
+      The word is two characters, 1 character, or 0 characters.
+      */
    if(length <= 2) {
       cout << suffix << word << endl;
       if(length == 2)
@@ -20,6 +23,7 @@ void ana(const string& suffix, const string& word) {
       ana(suffix + word.substr(i, 1), word.substr(0, i) + word.substr(i + 1, length - 1));
 }
 
+/* Wrapper function. */
 void anagram(const string& word) {
    size_t length = word.length();
 
